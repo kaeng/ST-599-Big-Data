4ba61bc4d26a6a09fe41c42fedf43aacfdfe3d44
kaeng <engka@science.oregonstate.edu>
2014-04-07 14:52:48 -0700

    Upload Team Expectations contract...

---
+++ Team Expectations Contract.R
@@ -0,0 +1,25 @@
+#===============================
+# Project 1: American Community Survey (ACS)
+# Presentation Date: Weds 4/23/14
+# GROUP EXPECTATIONS CONTRACT
+#===============================
+
+We will show up to meetings on time. If a conflict arises, we will inform another member 
+of the team as soon as possible.
+
+If we are having a difficult time completing our agreed upon task(s) before the next team 
+meeting, we will discuss our difficulty with another team member and try to resolve it 
+before the meeting.
+
+Timely and clear communication. Respond to group emails within 24 hours, and if you know
+you will be disconnected from email for an extended period of time, alert the group at least
+24 hours before hand.
+
+Read meeting minutes after each meeting once posted, especially if you were not able to
+attend the group meeting.
+
+Disagreements should be compromised civilly between group members. A group meeting should be
+called if differences cannot be resolved.
+
+Committed changes to the git hub should be done clearly - clean code and include comments,
+name files appropriately, and save them in the proper folder/location if applicable.
