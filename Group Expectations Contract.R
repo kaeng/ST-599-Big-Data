0434b3326a1cb44ada2b0b649083c70e2c203eae
kaeng <engka@science.oregonstate.edu>
2014-04-07 14:05:43 -0700

    minutes 4/7/14

---
+++ Group Expectations Contract.R
@@ -0,0 +1,6 @@
+#===============================
+# Project 1: American Community Survey (ACS)
+# Presentation Date: Weds 4/23/14
+# GROUP EXPECTATIONS CONTRACT
+#===============================
+
---
+++ Minutes/Minutes 04072014.R
@@ -0,0 +1,68 @@
+#===============================
+# Project 1: American Community Survey (ACS)
+# Presentation Date: Weds 4/23/14
+# Meeting Notes
+# 4/7/14
+#===============================
+
+# 1) Group Members assigned to roles
+  Coordinator - Lu Wang
+  Recorder - Katie Eng
+  Monitor - Fang Wu Wei
+  Checker - Ben Brintz
+
+# 2) Relevant links
+Group Github:
+  https://github.com/kaeng/ST-599-Big-Data
+
+Data can be found here:
+  http://www.census.gov/acs/www/data_documentation/pums_data/
+  
+Data dictionary (list of variable names and codings):
+  http://www.census.gov/acs/www/data_documentation/pums_documentation/
+  
+Survey questions avaliable online here: # Note: this is different than the first link sent
+  # the question numbers in the first link sent do not match up to the survey distributed in class
+  http://www.census.gov/acs/www/about_the_survey/explore_the_form/
+  
+#3) Proposed research questions, so far
+  - Question 21: What is the annual payment for fire, hazard, and flood insurance on THIS property?
+
+  * Does Oregon have higher / lower reported insurance coverage than California?
+  (Even though we cannot compare the different types of insurance, total insurance payments can
+   be compared.)
+
+
+  - Question 11: At this house, apartment, or movile home - do you or any member of this household
+  subscribe to the Internet using - dial up, DSL, cable modem, fiber optic, mobile broadband,
+  satellite internet, other. (Each internet service method has a check box for yes/no, while "other"
+  has a fill-in-the-blank.)
+
+  * How is the proportion of respondants subscribing to dial-up, etc. methods change over time?
+    (Oregon? Oregon vs. California?)
+  
+
+  - Question 14: Last month, what was the cost of ELECTRICITY for this house, apartment, or
+  mobile home? (Dollar response, unless "included in rent fee" or "no charge or electricity
+  not used" options sected.) Last month, what was the cost of GAS for this house, apartment, or
+  mobile home? (Dollar response, unless "included in rent fee" or "included in electricity payment"
+  or"no charge or electricity not used" options sected.)
+
+  * How to electricity and gas costs compare? (Oregon? Oregon vs. California?)
+
+
+#4) Started drafting Team Expectations contract.
+Draft in github as .R file.
+
+
+# ============
+# TO DO LIST
+# ============
+
+- Email entire group one proposed question of interest by 4/8/14 at 8:00pm
+  - Ben, compile all questions and bring them to class?
+  - On Weds, 4/9/14, we will share/discuss our ideas with the class
+
+- Work on updating Team Expectations contract.
+  - Ben will check and print this before submitting it on Weds, 4/9/14
+  - Katie has posted a draft on the github (4/7/14)
