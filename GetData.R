98961104a9cb0f7b0b66bfae071e62bb1f0cfa93
Ben Brintz <benbrintz@10-196-20-142.wireless.oregonstate.edu>
2014-04-11 14:25:30 -0700

    Test loading OR 5-year housing data 2008-20012 in R

--- GetData.R
+++ GetData.R
@@ -7,22 +7,17 @@
 download.file("http://www2.census.gov/acs2012_5yr/pums/csv_hor.zip", 
               destfile = "csv_hor.zip")
 
-# download CA 2012_5yr
-download.file("http://www2.census.gov/acs2012_5yr/pums/csv_hca.zip", 
-              destfile = "csv_hca.zip")
+
 
 # === unzip and get file === #
 # look into others function: unz, unzip, system('unzip ...'), 
 # check files in the zip archive without extracting
 unzip("csv_hor.zip", list = TRUE)
-unzip("csv_hca.zip", list = TRUE)
+
 
 # extract and read in one go
-or_sm <- read.csv(unz("csv_hor.zip", "ss12hor.csv"),  # notice just 10 lines! why?
-                  stringsAsFactors = FALSE)
-str(or_sm)
+or_sm <- read.csv(unz("csv_hor.zip", "ss12hor.csv"),stringsAsFactors = FALSE)[,c(2,54)]
 
 
-ca_sm <- read.csv(unz("csv_hca.zip", "ss12hca.csv"),  # notice just 10 lines! why?
-                  stringsAsFactors = FALSE)
-str(ca_sm)
+
+
