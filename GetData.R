6888c4e042b01bce3bb9b1d4d7511ed63ac25d1c
BJB2088 <BJB2088@gmail.com>
2014-04-13 09:01:49 -0700

    Added the 2012 5 year personal and 2007 3 year household and personal datasets

--- GetData.R
+++ GetData.R
@@ -15,13 +15,21 @@ unzip("csv_hor.zip", list = TRUE)
 
 
 # extract and read in one go
-or_sm <- read.csv(unz("csv_hor.zip", "ss12hor.csv"),stringsAsFactors = FALSE)[,c(2,54)]
+or_sm <- read.csv(unz("csv_hor.zip", "ss12hor.csv"),stringsAsFactors = FALSE)[,c(2,9,54)]
 
 
 ## BEN
+download.file("http://www2.census.gov/acs2012_5yr/pums/csv_por.zip", 
+              destfile = "csv_por.zip")
+por_twelve <- read.csv(unz("csv_por.zip", "ss12por.csv"),stringsAsFactors = FALSE)[,c(2,76,77)]
 
+download.file("http://www2.census.gov/acs2007_3yr/pums/csv_hor.zip", 
+              destfile = "csv_horsev.zip")
+hor_seven <- read.csv(unz("csv_horsev.zip", "ss07hor.csv"),stringsAsFactors = FALSE)[,c(2,8,49)]
 
-
+download.file("http://www2.census.gov/acs2007_3yr/pums/csv_por.zip", 
+              destfile = "csv_porsev.zip")
+por_seven <- read.csv(unz("csv_porsev.zip", "ss07por.csv"),stringsAsFactors = FALSE)[,c(2,59,60)]
 ## LU
 
 
