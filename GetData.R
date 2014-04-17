e397f5f3a76dbbb08762286706e7376c33623b37
wfw119 <wfw119@gmail.com>
2014-04-17 15:17:27 -0700

    Update GetData.R

--- GetData.R
+++ GetData.R
@@ -157,11 +157,51 @@ por_2001 <- read.csv(unz("csv_por_2001.zip", "ss01por.csv"),stringsAsFactors = F
 # download OR 2000 housing #
 download.file("http://www2.census.gov/acs/downloads/pums/2000/csv_hor.zip", destfile = "csv_hor_2000.zip")
 unzip("csv_hor_2000.zip", list = TRUE)
-hor_2000 <- read.csv(unz("csv_hor_2000.zip", "ss00hor.csv"),stringsAsFactors = FALSE)[,c(2,9,56)]
+hor_2000 <- read.csv(unz("csv_hor_2000.zip", "c2sshor.csv"),stringsAsFactors = FALSE)[,c(2,9,56)]
 # extract data from three columns: "SERIALNO" "ADJUST" "HINCP"
 
 # download OR 2000 personal #
 download.file("http://www2.census.gov/acs/downloads/pums/2000/csv_por.zip", destfile = "csv_por_2000.zip")
 unzip("csv_por_2000.zip", list = TRUE)
-por_2002 <- read.csv(unz("csv_por_2000.zip", "ss00por.csv"),stringsAsFactors = FALSE)[,c(3,56,57)]
+por_2000 <- read.csv(unz("csv_por_2000.zip", "c2sspor.csv"),stringsAsFactors = FALSE)[,c(3,56,57)]
 # extract data from three columns: "SERIALNO" "WKHP" "WKL"
+
+library(dplyr)
+
+# extracting persons who worked last 12 months
+por_2000_1 <- filter(por_2000,WKL=="1")
+# group each person by SERIALNO that is household number
+household <- group_by(por_2000_1,SERIALNO)
+# creating a new data frame in terms of each household, including average working hours per household, highest education level per household, and number of members per household
+house_aggregation <- summarise(household,avg_hours=mean(WKHP,na.rm=TRUE),high_edu=max(SCHL,na.rm=TRUE),n=n())
+# extracting the housing data per household in terms of SERIALNO in new data frame "house_aggregation"
+hor_2000_1 <- hor_2000[hor_2000$SERIALNO %in% house_aggregation$SERIALNO,] 
+# merging the new household data frame with housing data based in terms of the unique SERIALNO
+data_2000 <- cbind(hor_2000_1,avg_hours=house_aggregation$avg_hours,high_edu=house_aggregation$high_edu,n=house_aggregation$n)
+# calculating household income per person
+data_2000 <- mutate(data_2000,avg_income=HINCP/n)
+
+# matching education level to code
+# codes from data dictionary for 2000-2002
+# I'm not sure the dictionary is the same for other years, please double check
+# You can find PUMS DATA DICTIONARY for specific time periods through this link: http://www.census.gov/acs/www/data_documentation/pums_documentation/
+EDU_codes <- c("bb"="N/A (less than 3 years old",
+               "01"="None",
+               "02"="Nursery school to grade 4",
+               "03"="Grade 5 or grade 6",
+               "04"="Grade 7 or grade 8",
+               "05"="Grade 9",
+               "06"="Grade 10",
+               "07"="Grade 11",
+               "08"="Grade 12 no diploma",
+               "09"="High school graduate",
+               "10"="Some college but no degree",
+               "11"="Vo/Tech/Bus school degree",
+               "12"="Associate degree in college",
+               "13"="Bachelor's degree",
+               "14"="Master's degree",
+               "15"="Professional school degree",
+               "16"="Doctorate degree")
+
+# adding education codes to data frame "data_2000"
+data_2000 <- mutate(data_2000,highest_education=EDU_codes[as.character(high_edu)])
