5eb65affa914ec7951b1bc983b6771001dc5a030
BJB2088 <BJB2088@gmail.com>
2014-04-19 10:33:02 -0700

    minutes commit

---
+++ Minutes/Minutes 04182014.R
@@ -0,0 +1,17 @@
+All members present.
+
+Settled on final research question: average $/hour made over time, by educational attainment.
+
+Each group member did appropriate calculations for the data sets originally assigned to them.
+
+Calculated means were merged together into one document.
+
+Report write-up. Contribute to google doc by Saturday night, so that editing and checking can take place Sunday.
+
+Background - Fang Wu
+Findings - Ben
+Obstacles - Katie
+Additional topics - Lu 
+
+
+Next meeting Monday at 1, to compile and look over presentation.
